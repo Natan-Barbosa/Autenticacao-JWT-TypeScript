fb5df0b39c857cacf339ace79c9b9be429025f90
natan <natanbarbosax193@gmail.com>
2025-07-29 12:05:09 -0300

    edit: add authentication logic

--- src/auth/auth.service.ts
+++ src/auth/auth.service.ts
@@ -1,4 +1,30 @@
-import { Injectable } from '@nestjs/common';
+import { BadRequestException, Injectable } from '@nestjs/common';
+import { UsersService } from 'src/users/users.service';
+import { AuthDto } from './auth.dto';
+import { JwtService } from '@nestjs/jwt';
 
 @Injectable()
-export class AuthService {}
+export class AuthService {
+  constructor(
+    private usersService: UsersService,
+    private readonly jwtService: JwtService,
+  ) {}
+
+  public async signIn(dto: AuthDto) {
+    const user = this.usersService.findOne(dto.email);
+    if (user.password != dto.password) {
+      throw new BadRequestException(
+        'Invalid Credentials, Verify Password Or UserName',
+      );
+    }
+    const payload = {
+      sub: user.userId,
+      email: user.email,
+      userRole: user.userRole,
+    };
+
+    return {
+      accessToken: await this.jwtService.signAsync(payload),
+    };
+  }
+}
