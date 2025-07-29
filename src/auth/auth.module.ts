f7d219d94ee7dae62edf3e420d27839fdbcdfeb6
natan <natanbarbosax193@gmail.com>
2025-07-29 11:30:04 -0300

    edit: add dot env config and jwt dependency

--- src/auth/auth.module.ts
+++ src/auth/auth.module.ts
@@ -2,9 +2,16 @@ import { Module } from '@nestjs/common';
 import { AuthController } from './auth.controller';
 import { AuthService } from './auth.service';
 import { UsersModule } from 'src/users/users.module';
+import { JwtModule } from '@nestjs/jwt';
+import * as dotenv from 'dotenv';
+
+dotenv.config();
 
 @Module({
-  imports: [UsersModule],
+  imports: [
+    UsersModule,
+    JwtModule.register({ secret: process.env.JWT_SECRET }),
+  ],
   controllers: [AuthController],
   providers: [AuthService],
 })
