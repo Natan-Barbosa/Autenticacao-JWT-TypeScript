4943054b30f0eef13d8f5aca4882a2de2212566f
natan <natanbarbosax193@gmail.com>
2025-07-29 11:23:03 -0300

    edit: add validation case user not exists and switch name to email

--- src/users/users.service.ts
+++ src/users/users.service.ts
@@ -1,21 +1,26 @@
-import { Injectable } from '@nestjs/common';
+import { BadRequestException, Injectable } from '@nestjs/common';
 
 @Injectable()
 export class UsersService {
   private readonly users = [
     {
       userId: 1,
-      username: 'Teste 1',
+      email: 'teste1@gmail.com',
       password: '12345678',
     },
     {
       userId: 2,
-      username: 'Teste 2',
+      email: 'teste2@gmail.com',
       password: '87654321',
     },
   ];
 
-  public findOne(userID: number) {
-    return this.users.find((user) => user.userId == userID);
+  public findOne(email: string) {
+    const user = this.users.find((user) => user.email == email);
+
+    if (!user) {
+      throw new BadRequestException('User Not Found');
+    }
+    return user;
   }
 }
