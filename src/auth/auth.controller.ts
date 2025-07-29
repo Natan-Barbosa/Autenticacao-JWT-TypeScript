ff84b397720d6d5877d5d44d78eaeaafabc29fd2
natan <natanbarbosax193@gmail.com>
2025-07-29 12:53:54 -0300

    edit: add private and public routes

--- src/auth/auth.controller.ts
+++ src/auth/auth.controller.ts
@@ -1,13 +1,43 @@
-import { Body, Controller, Post } from '@nestjs/common';
+import {
+  Body,
+  Controller,
+  Get,
+  Post,
+  Request,
+  UseGuards,
+} from '@nestjs/common';
 import { AuthService } from './auth.service';
 import { AuthDto } from './auth.dto';
+import { Request as ExpressRequest } from 'express';
+import { AuthGuard } from './auth.guard';
+
+interface AuthenticatedRequest extends ExpressRequest {
+  user: {
+    sub: string;
+    email: string;
+    userRole: string;
+    iat?: number;
+    exp?: number;
+  };
+}
 
 @Controller('auth')
 export class AuthController {
   constructor(private authService: AuthService) {}
 
-  @Post()
+  @Post('/login')
   public signIn(@Body() dto: AuthDto) {
     return this.authService.signIn(dto);
+  }
+
+  @Get('/private/profile')
+  @UseGuards(AuthGuard)
+  public getProfile(@Request() req: AuthenticatedRequest) {
+    return req.user;
+  }
+
+  @Get('/public/profile')
+  public getPublicInfo() {
+    return 'This Is A Public Route';
   }
 }
